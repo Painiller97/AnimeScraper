47f33304db3c87a0bfe6e9ff580957c22b8dd328
BuildTools <unconfigured@null.spigotmc.org>
2018-04-20 18:58:19 +0200

    Guardar cambios

--- src/main/java/com/mycompany/animescraping/Main.java
+++ src/main/java/com/mycompany/animescraping/Main.java
@@ -5,13 +5,31 @@
  */
 package com.mycompany.animescraping;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  *
  * @author Pues4Aula4
  */
 public class Main {
-    
-    public static void main(){
+
+    public static void main(String[] args) throws IOException {
+        
+        
+        for (int i = 1; i <= 47; i++) {
+            Document doc = Jsoup.connect("https://www.animeid.tv/series?sort=asc&pag="+i).get();
+            Elements elements = doc.select("#result > article > a > header");
+        
+        for (Element element : elements) {
+            System.out.println(element.text());
+        }
+        }
+        
+        
     }
-    
+
 }
